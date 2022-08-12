53d41d293340abe8fe9674aa311dca80fd90c174
shpvsk <mshpvsk@yandex.ru>
2022-08-13 01:09:37 +0300

    feat: implemented the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  while (str.split('')) {
+    // length of the string at the beginning of each iteration
+    let oldLength = str.length
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      // remove all characters from the string that are in the config
+      str = str.replace(bracketsConfig[i].join(''), '')
+    }
+
+    // сравнить длину строки после удаления из нее символов из конфига
+    if (str.length === oldLength) return str.length === 0
+  }
 }
